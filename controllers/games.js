4c733e12ffccab3685b0d3741a9fc68dd4d761d4
Victor Johnson <vjohnson22@gmail.com>
2019-07-30 16:38:52 -0400

    updated games data model

---
+++ controllers/games.js
@@ -0,0 +1,20 @@
+const Game = require('../db/models')
+
+module.exports = {
+    index: (req,res) => {
+
+    },    
+    show: (req,res) => {
+
+    },
+    create: (req,res) => {
+
+    },
+    update: (req,res) => {
+
+    },
+    destroy: (req,res) => {
+
+    },
+}
+
--- db/models/games.js
+++ db/models/games.js
@@ -4,19 +4,14 @@ const Schema = mongoose.Schema
 const GameSchema = new Schema( {
     age_ratings:String,
     aggregated_rating: Number,
-    artworks:[String],
     first_release_date: Date,
     franchise: String,
     game_engines: [Number],    
-    genres: [Number],    
     involved_companies: [Number],    
     name: String,
-    platforms: [Number],
     popularity: Number,
-    release_dates: [Date],
     storyline: String,
     summary: String,
-    themes: [Number],
     url: String
 })
 
