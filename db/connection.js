e98f2eafa9cc8b33aabfaa84e2062df7013b6684
Victor Johnson <vjohnson22@gmail.com>
2019-08-02 05:01:44 -0400

    updated db

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,19 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/subsidies", {useNewUrlParser: true});
+let mongoURI =""
+
+
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+  } else {
+    mongoURI = "mongodb://localhost/subsidies";
+  }
+  mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(instance =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch(error => console.log("Connection failed!", error));
 
 mongoose.Promise = Promise;
 
