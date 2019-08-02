486fce6b0fa24e2794a929325e82ba31dd06004e
Victor Johnson <vjohnson22@gmail.com>
2019-08-02 09:00:03 -0400

    updates to connection to try to rebuild db. if it doesnt work, go back

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,49 @@
-const mongoose = require("mongoose");
 
-let mongoURI =""
+const mongoose = require('mongoose')
 
+mongoose.Promise = Promise
+
+let mongoURI = "";
 
 if (process.env.NODE_ENV === "production") {
-    mongoURI = process.env.DB_URL;
-  } else {
-    mongoURI = "mongodb://localhost/subsidies";
-  }
-  mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
-  .then(instance =>
-    console.log(`Connected to db: ${instance.connections[0].name}`)
-  )
-  .catch(error => console.log("Connection failed!", error));
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://localhost/subsidies";
+}
 
-mongoose.Promise = Promise;
+// connect to the database, with the imported mongoose instance
+mongoose
+  .connect(mongoURI, {useNewUrlParser: true})
+  .then(instance => console.log(`Connected to db: ${instance.connections[0].name}`))
+  .catch(error => console.log('Connection failed!', error))
 
-module.exports = mongoose;
+// now, our mongoose instance has a configured connection to our local db, in addition
+// to its model configuration
+module.exports = mongoose
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// let mongoURI =""
+
+
+// if (process.env.NODE_ENV === "production") {
+//     mongoURI = process.env.DB_URL;
+//   } else {
+//     mongoURI = "mongodb://localhost/subsidies";
+//   }
+//   mongoose
+//   .connect(mongoURI, { useNewUrlParser: true })
+//   .then(instance =>
+//     console.log(`Connected to db: ${instance.connections[0].name}`)
+//   )
+//   .catch(error => console.log("Connection failed!", error));
+
+// mongoose.Promise = Promise;
+
+// module.exports = mongoose;
--- index.js
+++ index.js
@@ -10,9 +10,7 @@ app.use('/schools',require('./routes/schools'))
 app.use('/headcoach',require('./routes/headCoach'))
 app.use('/asstcoach', require('./routes/asstCoach'))
 
-app.listen(6000, () => console.log("listening on port 5000"))
-
-app.set("port", process.env.PORT || 000);
+app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
